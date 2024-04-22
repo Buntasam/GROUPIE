10c761b50499156ce1425d85d9145f8535007186
Buntasam <144217405+Buntasam@users.noreply.github.com>
2024-04-22 10:28:43 +0200

    1

--- main.go
+++ main.go
@@ -1,23 +1,21 @@
-package controller
+package main
 
 import (
 	"fmt"
 	"net/http"
-	
 )
 
 func main() {
-
 	colorGreen := "\033[32m" // Mise en place de couleur pour la lisibilité dans le terminal
 	colorBlue := "\033[34m"
 	colorYellow := "\033[33m"
 
-	fmt.Println(string(colorBlue), "[SERVER_INFO] : Starting local Server...") // Information lorsque que le serveur est lancé
+	fmt.Println(string(colorBlue), " Starting local Server...") // Information lorsque que le serveur est lancé
 
 	fs := http.FileServer(http.Dir("static")) // Mise en place d'un dossier qui contient le CSS/IMG/JS
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	// Création des différentes routes, pour y accéder le serveur va chercher le package "controller" dans les dossiers et exécuter la fonction demandé.
+	// Création des différentes routes, pour y accéder le serveur va chercher le package "controller" dans les dossiers et exécuter la fonction demandée.
 	//http.HandleFunc("/api/relation/", controller.RelationData)
 	//http.HandleFunc("/map", controller.Map)
 	//http.HandleFunc("/search", controller.Search)
@@ -27,24 +25,12 @@ func main() {
 	//http.HandleFunc("/api/relation", controller.Relation)
 	//http.HandleFunc("/", controller.Accueil)
 
-	fmt.Println(string(colorGreen), "[SERVER_READY] : on http://localhost:8000 ✅ ")    // Mise en place de l'URL pour l'utilisateur
-	fmt.Println(string(colorYellow), "[SERVER_INFO] : To stop the program : Ctrl + c") // Information pour couper le serveur
-	http.ListenAndServe(":8000", nil)                                                  // Mise en place du port
+	// Ajout de la route pour l'index.html
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	fmt.Println(string(colorGreen), " http://localhost:8000 ✅ ")        // Mise en place de l'URL pour l'utilisateur
+	fmt.Println(string(colorYellow), " To stop the program : Ctrl + c") // Information pour couper le serveur
+	http.ListenAndServe(":8000", nil)                                   // Mise en place du port
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
